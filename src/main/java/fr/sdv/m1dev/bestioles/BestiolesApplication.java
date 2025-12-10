a8521fb66b26886db6fa37a6cd8a534d64b57f2e
E207838L <milletarthur4@gmail.com>
2025-12-10 16:29:58 +0100

    Fin TP3

--- src/main/java/fr/sdv/m1dev/bestioles/BestiolesApplication.java
+++ src/main/java/fr/sdv/m1dev/bestioles/BestiolesApplication.java
@@ -14,39 +14,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.List;
 
 @SpringBootApplication
-public class BestiolesApplication implements CommandLineRunner {
-
-    @Autowired
-    private AnimalRepository animalRepository;
-
-    @Autowired
-    private SpeciesRepository speciesRepository;
-
-    @Autowired
-    private PersonRepository personRepository;
+public class BestiolesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BestiolesApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        animalRepository.findAll().forEach(System.out::println);
 
-        Species s = new Species("Triceratops", "Triceratops horridus");
-        speciesRepository.save(s);
-        Animal a = new Animal("Noir", "Tritri", "M", s);
-        animalRepository.save(a);
-        animalRepository.findById(a.getId()).ifPresent(System.out::println);
-        Person p = new Person(25,"Doe", "John","jean","test",true, List.of(),List.of(a));
-        personRepository.save(p);
-        System.out.println("Nb animaux: " + animalRepository.findAll().size());
-        animalRepository.findAll().forEach(animal ->
-                System.out.println(animal.getId() + " - " + animal.getName() + " - " + animal.getSpecies().getCommonName())
-        );
-        personRepository.delete(p);
-        animalRepository.delete(a);
-        speciesRepository.delete(s);
-
-    }
 }
---
+++ src/main/java/fr/sdv/m1dev/bestioles/boot/StartUp.java
@@ -0,0 +1,48 @@
+package fr.sdv.m1dev.bestioles.boot;
+
+import fr.sdv.m1dev.bestioles.dal.AnimalRepository;
+import fr.sdv.m1dev.bestioles.dal.PersonRepository;
+import fr.sdv.m1dev.bestioles.dal.SpeciesRepository;
+import fr.sdv.m1dev.bestioles.domain.Animal;
+import fr.sdv.m1dev.bestioles.domain.Person;
+import fr.sdv.m1dev.bestioles.domain.Species;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.context.ApplicationContext;
+
+
+import java.util.List;
+@Component
+public class StartUp implements CommandLineRunner {
+
+    @Autowired
+    private AnimalRepository animalRepository;
+
+    @Autowired
+    private SpeciesRepository speciesRepository;
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        animalRepository.findAll().forEach(System.out::println);
+
+        Species s = new Species("Triceratops", "Triceratops horridus");
+        speciesRepository.save(s);
+        Animal a = new Animal("Noir", "Tritri", "M", s);
+        animalRepository.save(a);
+        animalRepository.findById(a.getId()).ifPresent(System.out::println);
+        Person p = new Person(25,"Doe", "John","jean","test",true, List.of(),List.of(a));
+        personRepository.save(p);
+        System.out.println("Nb animaux: " + animalRepository.findAll().size());
+        animalRepository.findAll().forEach(animal ->
+                System.out.println(animal.getId() + " - " + animal.getName() + " - " + animal.getSpecies().getCommonName())
+        );
+        personRepository.delete(p);
+        animalRepository.delete(a);
+        speciesRepository.delete(s);
+
+    }
+}
